c9bd91b858b6770fc99916a4868562f063ada7a9
Dharm Patel <142787486+Dharm2412@users.noreply.github.com>
2024-05-30 18:43:18 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() { 
+        return (
+            <div className='header'>
+                <div>
+                    <h1>Netflix Top 10</h1>
+                </div>
+            </div>
+            
+        );
+    }
+}
+ 
+export default Header;
